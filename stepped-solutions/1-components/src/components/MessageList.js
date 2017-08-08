62fa326d79b4c2aa78d8961e629a0d9b1459714f
Danielle Penny <danielle.penny@doordash.com>
2017-08-08 13:58:41 -0700

    first components, trash css

---
+++ stepped-solutions/1-components/src/components/MessageList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        <p> Here's a message </p>
+        <p> Here's another! </p>
+      </div>
+    )
+  }
+}
+
+export default MessageList;
---
+++ stepped-solutions/1-components/src/components/RoomHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class RoomHeader extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h2>Fun Chat room</h2>
+        <h3> Arthur, Buster, Muffy </h3>
+      </div>
+    )
+  }
+}
+
+export default RoomHeader;
